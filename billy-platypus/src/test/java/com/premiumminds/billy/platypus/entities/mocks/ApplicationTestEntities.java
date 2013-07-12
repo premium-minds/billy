ddd2a80fe18573769e856c9c8418cf2e73400e84
Hugo Correia <hfcorreia91@gmail.com>
2013-07-12 16:09:32 +0100

    Update with maven-licence-plugin support.
    Automatically injects license preamble in source files

--- billy-platypus/src/test/java/com/premiumminds/billy/platypus/entities/mocks/ApplicationTestEntities.java
+++ billy-platypus/src/test/java/com/premiumminds/billy/platypus/entities/mocks/ApplicationTestEntities.java
@@ -1,21 +1,21 @@
-/*******************************************************************************
+/**
  * Copyright (C) 2013 Premium Minds.
- *  
- * This file is part of billy-platypus.
- * 
- * billy-platypus is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * billy-platypus is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
+ *
+ * This file is part of billy.
+ *
+ * billy is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * billy is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy-platypus.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * along with billy. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.premiumminds.billy.platypus.entities.mocks;
 
 import java.util.ArrayList;
