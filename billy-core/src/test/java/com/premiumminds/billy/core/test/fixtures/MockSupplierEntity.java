f19c80640d4d9d33b991107cf37b8caa1fe6b825
Hugo Correia <hfcorreia91@gmail.com>
2013-07-12 12:12:47 +0100

    Add licence preamble

--- billy-core/src/test/java/com/premiumminds/billy/core/test/fixtures/MockSupplierEntity.java
+++ billy-core/src/test/java/com/premiumminds/billy/core/test/fixtures/MockSupplierEntity.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2013 Premium Minds.
+ *  
+ * This file is part of billy-core.
+ * 
+ * billy-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * billy-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with billy-core.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package com.premiumminds.billy.core.test.fixtures;
 
 import java.util.ArrayList;
@@ -13,16 +31,18 @@ import com.premiumminds.billy.core.services.entities.Contact;
 public class MockSupplierEntity extends MockBaseEntity implements
 		SupplierEntity {
 
-	private String name;
-	private String taxRegistrationNumber;
-	private Address mainAddress;
-	private Address billingAddress;
-	private Address shippingAddress;
-	private Contact mainContact;
-	private boolean selfBillingAgreement;
-	private List<Address> addresses;
-	private List<Contact> contacts;
-	private List<BankAccount> bankAccounts;
+	private static final long serialVersionUID = 1L;
+
+	public String name;
+	public String taxRegistrationNumber;
+	public Address mainAddress;
+	public Address billingAddress;
+	public Address shippingAddress;
+	public Contact mainContact;
+	public boolean selfBillingAgreement;
+	public List<Address> addresses;
+	public List<Contact> contacts;
+	public List<BankAccount> bankAccounts;
 
 	public MockSupplierEntity() {
 		this.addresses = new ArrayList<Address>();
