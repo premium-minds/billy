97b24fe7b3fbf5ad30d6bf4ef001a7d17f362bff
Hugo Correia <hfcorreia91@gmail.com>
2013-07-12 10:59:38 +0100

    Change test to use context.yml

--- billy-core/src/test/java/com/premiumminds/billy/core/test/services/builders/TestContextBuilder.java
+++ billy-core/src/test/java/com/premiumminds/billy/core/test/services/builders/TestContextBuilder.java
@@ -1,49 +1,41 @@
 package com.premiumminds.billy.core.test.services.builders;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.premiumminds.billy.core.persistence.dao.DAOContext;
-import com.premiumminds.billy.core.persistence.entities.ContextEntity;
 import com.premiumminds.billy.core.services.entities.Context;
 import com.premiumminds.billy.core.test.AbstractTest;
 import com.premiumminds.billy.core.test.fixtures.MockContextEntity;
 
 public class TestContextBuilder extends AbstractTest {
-	
-	private static final String NAME = "name";
-	private static final String DESCRIPTION = "description";
-	
+
+	private static final String CONTEXT_YML = "src/test/resources/Context.yml";
+
 	@Test
 	public void doTest() {
-		MockContextEntity mockContext = this.loadFixture(ContextEntity.class);
-		
+		MockContextEntity mockContext = (MockContextEntity) createMockEntityFromYaml(
+				MockContextEntity.class, CONTEXT_YML);
+
 		DAOContext mockDaoContext = this.getMock(DAOContext.class);
-		
-		Mockito.when(mockDaoContext.getEntityInstance()).thenReturn(new MockContextEntity());
-		
+
+		Mockito.when(mockDaoContext.getEntityInstance()).thenReturn(
+				new MockContextEntity());
+
 		Context.Builder builder = new Context.Builder(mockDaoContext);
-		
-		builder.setDescription(mockContext.getDescription()).setName(mockContext.getName()).setParentContextUID(null);
-		
+
+		builder.setDescription(mockContext.getDescription())
+				.setName(mockContext.getName()).setParentContextUID(null);
+
 		Context context = builder.build();
-		
-		assert(context != null);
-		assertEquals(NAME, context.getName());
-		assertEquals(DESCRIPTION, context.getDescription());
-		assert(context.getParentContext() == null);
-		
-	}
-	
-	public MockContextEntity loadFixture(Class<ContextEntity> clazz) {
-		MockContextEntity result = new MockContextEntity();
-		
-		result.name = NAME;
-		result.description = DESCRIPTION;
-		
-		return result;
+
+		assert (context != null);
+		assertEquals(mockContext.getName(), context.getName());
+		assertEquals(mockContext.getDescription(), context.getDescription());
+		assert (context.getParentContext() == null);
+
 	}
 
 }
