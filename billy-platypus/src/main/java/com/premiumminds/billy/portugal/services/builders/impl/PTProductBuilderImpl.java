f18e17a6717d340cbe3844b32ed9134facfd9966
Antonio Monteiro <antonio.monteiro@premium-minds.com>
2013-07-18 15:06:34 +0100

    Fix validations in PTProductBuilderImpl

--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTProductBuilderImpl.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTProductBuilderImpl.java
@@ -1,20 +1,21 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy platypus (PT Pack).
- *
- * billy platypus (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy platypus (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy platypus (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy platypus (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy platypus (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy platypus (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.services.builders.impl;
 
@@ -22,6 +23,7 @@ import javax.inject.Inject;
 
 import com.premiumminds.billy.core.exceptions.BillyValidationException;
 import com.premiumminds.billy.core.services.builders.impl.ProductBuilderImpl;
+import com.premiumminds.billy.core.util.BillyValidator;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTProduct;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTTax;
 import com.premiumminds.billy.portugal.persistence.entities.PTProductEntity;
@@ -43,8 +45,18 @@ public class PTProductBuilderImpl<TBuilder extends PTProductBuilderImpl<TBuilder
 	}
 
 	@Override
-	protected void validateInstance() throws BillyValidationException {
-		super.validateInstance();
+	public TBuilder setNumberCode(String code) {
+		BillyValidator.mandatory(code,
+				PTProductBuilderImpl.LOCALIZER.getString("field.number_code"));
+		this.getTypeInstance().setNumberCode(code);
+		return this.getBuilder();
 	}
 
+	@Override
+	protected void validateInstance() throws BillyValidationException {
+		super.validateInstance();
+		PTProduct p = this.getTypeInstance();
+		BillyValidator.mandatory(p.getNumberCode(),
+				PTProductBuilderImpl.LOCALIZER.getString("field.number_code"));
+	}
 }
