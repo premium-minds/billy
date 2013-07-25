2a73b12a80127eba63c20dabf4dfde69efb19441
Antonio Monteiro <antonio.monteiro@premium-minds.com>
2013-07-25 15:40:27 +0100

    Start doing Taxes

--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/util/Taxes.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/util/Taxes.java
@@ -1,88 +1,93 @@
 package com.premiumminds.billy.portugal.util;
 
 import com.google.inject.Injector;
+import com.premiumminds.billy.portugal.Config;
+import com.premiumminds.billy.portugal.Config.Key;
+import com.premiumminds.billy.portugal.persistence.dao.DAOPTTax;
 import com.premiumminds.billy.portugal.services.entities.PTTax;
 
-public class Taxes { //TODO
-	
+public class Taxes { // TODO
+
 	public class Continent {
-		
+
 		public PTTax normal() {
-			//TODO
-			return null;
+			Config configuration = new Config();
+			DAOPTTax dao = getInstance(DAOPTTax.class);
+			return (PTTax) dao.get(configuration
+					.getUID(Key.Context.Portugal.Continental.VAT.NORMAL_UUID));
 		}
-		
+
 		public PTTax intermediate() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 		public PTTax reduced() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 	}
-	
+
 	public class Madeira {
-		
+
 		public PTTax normal() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 		public PTTax intermediate() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 		public PTTax reduced() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 	}
-	
+
 	public class Azores {
-		
+
 		public PTTax normal() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 		public PTTax intermediate() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 		public PTTax reduced() {
-			//TODO
+			// TODO
 			return null;
 		}
-		
+
 	}
 
 	public PTTax exempt() {
-		//TODO
+		// TODO
 		return null;
 	}
-	
+
 	private final Continent continent;
 	private final Madeira madeira;
 	private final Azores azores;
 	private final Injector injector;
-	
+
 	public Taxes(Injector injector) {
 		continent = new Continent();
 		madeira = new Madeira();
 		azores = new Azores();
 		this.injector = injector;
 	}
-	
+
 	public Continent continent() {
 		return continent;
 	}
-	
+
 	public Madeira madeira() {
 		return madeira;
 	}
@@ -90,9 +95,9 @@ public class Taxes { //TODO
 	public Azores azores() {
 		return azores;
 	}
-	
+
 	private <T> T getInstance(Class<T> clazz) {
 		return injector.getInstance(clazz);
 	}
-	
+
 }
