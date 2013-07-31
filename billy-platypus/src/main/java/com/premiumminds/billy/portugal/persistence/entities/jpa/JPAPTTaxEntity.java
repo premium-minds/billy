cbca8fd2d353c416f0eedaf317fd52630ddef604
Antonio Monteiro <antonio.monteiro@premium-minds.com>
2013-07-31 18:09:54 +0100

    Add PTVATCode enum to PTTax

--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/persistence/entities/jpa/JPAPTTaxEntity.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/persistence/entities/jpa/JPAPTTaxEntity.java
@@ -1,24 +1,28 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy platypus (PT Pack).
- *
- * billy platypus (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy platypus (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy platypus (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy platypus (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy platypus (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy platypus (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.persistence.entities.jpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 import com.premiumminds.billy.core.persistence.entities.jpa.JPATaxEntity;
@@ -32,11 +36,23 @@ public class JPAPTTaxEntity extends JPATaxEntity implements PTTaxEntity {
 
 	private static final long serialVersionUID = 1L;
 
+	@Enumerated(EnumType.STRING)
+	@Column(name = "VAT_CODE")
+	private PTVATCode vatCode;
+
 	@Override
 	public Context getContext() {
 		return super.getContext();
 	}
-	
-	
+
+	@Override
+	public PTVATCode getVATCode() {
+		return vatCode;
+	}
+
+	@Override
+	public void setVATCode(PTVATCode code) {
+		this.vatCode = code;
+	}
 
 }
--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTTaxBuilderImpl.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTTaxBuilderImpl.java
@@ -1,20 +1,21 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy platypus (PT Pack).
- *
- * billy platypus (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy platypus (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy platypus (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy platypus (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy platypus (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy platypus (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.services.builders.impl;
 
@@ -22,6 +23,7 @@ import javax.inject.Inject;
 
 import com.premiumminds.billy.core.exceptions.BillyValidationException;
 import com.premiumminds.billy.core.services.builders.impl.TaxBuilderImpl;
+import com.premiumminds.billy.core.util.BillyValidator;
 import com.premiumminds.billy.core.util.Localizer;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTRegionContext;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTTax;
@@ -29,6 +31,7 @@ import com.premiumminds.billy.portugal.persistence.entities.PTRegionContextEntit
 import com.premiumminds.billy.portugal.persistence.entities.PTTaxEntity;
 import com.premiumminds.billy.portugal.services.builders.PTTaxBuilder;
 import com.premiumminds.billy.portugal.services.entities.PTTax;
+import com.premiumminds.billy.portugal.services.entities.PTTax.PTVATCode;
 
 public class PTTaxBuilderImpl<TBuilder extends PTTaxBuilderImpl<TBuilder, TTax>, TTax extends PTTax>
 		extends TaxBuilderImpl<TBuilder, TTax> implements
@@ -43,15 +46,26 @@ public class PTTaxBuilderImpl<TBuilder extends PTTaxBuilderImpl<TBuilder, TTax>,
 	}
 
 	@Override
+	public TBuilder setVATCode(PTVATCode code) {
+		BillyValidator.mandatory(code, LOCALIZER.getString("field.VATCode"));
+		this.getTypeInstance().setVATCode(code);
+		return this.getBuilder();
+	}
+
+	@Override
 	protected PTTaxEntity getTypeInstance() {
 		return (PTTaxEntity) super.getTypeInstance();
 	}
 
 	@Override
-	protected void validateInstance() throws BillyValidationException{
+	protected void validateInstance() throws BillyValidationException {
 		PTTaxEntity e = this.getTypeInstance();
-		
-			if(!((DAOPTTax) daoTax).getTaxes((PTRegionContextEntity)e.getContext(),e.getValidFrom(), e.getValidTo(), e.getDescription()).isEmpty())
+		BillyValidator.mandatory(e.getVATCode(),
+				LOCALIZER.getString("field.VATCode"));
+
+		if (!((DAOPTTax) daoTax).getTaxes(
+				(PTRegionContextEntity) e.getContext(), e.getValidFrom(),
+				e.getValidTo(), e.getDescription()).isEmpty())
 			throw new BillyValidationException();
 		super.validateInstance();
 	}
