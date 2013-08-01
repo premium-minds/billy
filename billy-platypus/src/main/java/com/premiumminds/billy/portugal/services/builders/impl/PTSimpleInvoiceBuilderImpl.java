e1932e883dd4195b6a691b8ba6bfa31f43a95a44
Hugo Correia <hfcorreia91@gmail.com>
2013-08-01 14:23:46 +0100

    add verification

--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTSimpleInvoiceBuilderImpl.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTSimpleInvoiceBuilderImpl.java
@@ -1,24 +1,28 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy platypus (PT Pack).
- *
- * billy platypus (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy platypus (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy platypus (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy platypus (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy platypus (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy platypus (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.services.builders.impl;
 
+import java.math.BigDecimal;
+
 import com.premiumminds.billy.core.exceptions.BillyValidationException;
+import com.premiumminds.billy.core.util.BillyValidator;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTBusiness;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTCustomer;
 import com.premiumminds.billy.portugal.persistence.dao.DAOPTSimpleInvoice;
@@ -46,6 +50,11 @@ public class PTSimpleInvoiceBuilderImpl<TBuilder extends PTSimpleInvoiceBuilderI
 	@Override
 	protected void validateInstance() throws BillyValidationException {
 		super.validateInstance();
+		PTSimpleInvoiceEntity i = this.getTypeInstance();
+
+		BillyValidator.mandatory(
+				(i.getAmountWithTax().compareTo(new BigDecimal(1000)) == 1),
+				PTInvoiceBuilderImpl.LOCALIZER.getString("field.source_id"));
 	}
 
 }
