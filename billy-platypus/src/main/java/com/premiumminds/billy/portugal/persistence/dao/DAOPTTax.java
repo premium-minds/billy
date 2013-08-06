38d503a419e8e4c319b6b92f1a773ab4b3726237
Antonio Monteiro <antonio.monteiro@premium-minds.com>
2013-08-06 11:55:53 +0100

    Remove uid parameter from query

--- billy-platypus/src/main/java/com/premiumminds/billy/portugal/persistence/dao/DAOPTTax.java
+++ billy-platypus/src/main/java/com/premiumminds/billy/portugal/persistence/dao/DAOPTTax.java
@@ -1,20 +1,21 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy platypus (PT Pack).
- *
- * billy platypus (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy platypus (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy platypus (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy platypus (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy platypus (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy platypus (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.persistence.dao;
 
@@ -22,7 +23,6 @@ import java.util.Date;
 import java.util.List;
 
 import com.premiumminds.billy.core.persistence.dao.DAOTax;
-import com.premiumminds.billy.core.services.UID;
 import com.premiumminds.billy.portugal.persistence.entities.PTRegionContextEntity;
 import com.premiumminds.billy.portugal.persistence.entities.PTTaxEntity;
 import com.premiumminds.billy.portugal.persistence.entities.jpa.JPAPTTaxEntity;
@@ -33,7 +33,6 @@ public interface DAOPTTax extends DAOTax {
 	public PTTaxEntity getEntityInstance();
 
 	public List<JPAPTTaxEntity> getTaxes(PTRegionContextEntity context,
-			Date validFrom, Date validTo, UID uid);
+			Date validFrom, Date validTo);
 
-	public List<JPAPTTaxEntity> getAllTaxes();
 }
