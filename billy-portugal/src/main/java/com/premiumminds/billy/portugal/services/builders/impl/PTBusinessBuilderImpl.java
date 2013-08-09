277f2d7aca778d96370a53d218c753d28926c329
Hugo Correia <hfcorreia91@gmail.com>
2013-08-09 14:13:03 +0100

    Update context localizer

--- billy-portugal/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTBusinessBuilderImpl.java
+++ billy-portugal/src/main/java/com/premiumminds/billy/portugal/services/builders/impl/PTBusinessBuilderImpl.java
@@ -1,20 +1,21 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy portugal (PT Pack).
- *
- * billy portugal (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy portugal (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy portugal (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy portugal (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy portugal (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy portugal (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.services.builders.impl;
 
@@ -61,7 +62,8 @@ public class PTBusinessBuilderImpl<TBuilder extends PTBusinessBuilderImpl<TBuild
 	protected void validateInstance() throws BillyValidationException {
 		BusinessEntity b = this.getTypeInstance();
 		BillyValidator.mandatory(b.getOperationalContext(),
-				"field.business_context");
+				PTBusinessBuilderImpl.LOCALIZER
+						.getString("field.business_context"));
 		BillyValidator
 				.mandatory(b.getFinancialID(), PTBusinessBuilderImpl.LOCALIZER
 						.getString("field.financial_id"));
