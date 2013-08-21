3f76b857b2d4b91abd53409d9d1e25bac06a9f50
Hugo Correia <hfcorreia91@gmail.com>
2013-08-21 17:14:14 +0100

    Remove generic invoice service

--- billy-portugal/src/main/java/com/premiumminds/billy/portugal/util/PersistenceServices.java
+++ billy-portugal/src/main/java/com/premiumminds/billy/portugal/util/PersistenceServices.java
@@ -1,20 +1,21 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- *
+ * 
  * This file is part of billy portugal (PT Pack).
- *
- * billy portugal (PT Pack) is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * billy portugal (PT Pack) is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
+ * 
+ * billy portugal (PT Pack) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * billy portugal (PT Pack) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy portugal (PT Pack). If not, see <http://www.gnu.org/licenses/>.
+ * along with billy portugal (PT Pack). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.portugal.util;
 
@@ -23,7 +24,6 @@ import com.premiumminds.billy.core.persistence.services.PersistenceService;
 import com.premiumminds.billy.portugal.services.entities.PTBusiness;
 import com.premiumminds.billy.portugal.services.entities.PTCreditNote;
 import com.premiumminds.billy.portugal.services.entities.PTCustomer;
-import com.premiumminds.billy.portugal.services.entities.PTGenericInvoice;
 import com.premiumminds.billy.portugal.services.entities.PTInvoice;
 import com.premiumminds.billy.portugal.services.entities.PTProduct;
 import com.premiumminds.billy.portugal.services.entities.PTRegionContext;
@@ -33,7 +33,6 @@ import com.premiumminds.billy.portugal.services.entities.PTTax;
 import com.premiumminds.billy.portugal.services.persistence.PTBusinessPersistenceService;
 import com.premiumminds.billy.portugal.services.persistence.PTCreditNotePersistenceService;
 import com.premiumminds.billy.portugal.services.persistence.PTCustomerPersistenceService;
-import com.premiumminds.billy.portugal.services.persistence.PTGenericInvoicePersistenceService;
 import com.premiumminds.billy.portugal.services.persistence.PTInvoicePersistenceService;
 import com.premiumminds.billy.portugal.services.persistence.PTProductPersistenceService;
 import com.premiumminds.billy.portugal.services.persistence.PTRegionContextPersitenceService;
@@ -93,14 +92,6 @@ public class PersistenceServices {
 	 */
 	public PersistenceService<PTTax> tax() {
 		return new PTTaxPersistenceService<PTTax>(this.injector);
-	}
-
-	/**
-	 * @return {@link PTGenericInvoice}.
-	 */
-	public PersistenceService<PTGenericInvoice> genericInvoice() {
-		return new PTGenericInvoicePersistenceService<PTGenericInvoice>(
-				this.injector);
 	}
 
 	/**
