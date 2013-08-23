5648ce0e951c894ece133db44cbdb76897238071
Hugo Correia <hfcorreia91@gmail.com>
2013-08-23 16:03:47 +0100

    Add builder manager

--- billy-core/src/main/java/com/premiumminds/billy/core/services/builders/impl/BuilderManager.java
+++ billy-core/src/main/java/com/premiumminds/billy/core/services/builders/impl/BuilderManager.java
@@ -1,39 +1,35 @@
 /**
  * Copyright (C) 2013 Premium Minds.
- * 
+ *
  * This file is part of billy core.
- * 
+ *
  * billy core is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * billy core is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with billy core. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.premiumminds.billy.core.util;
+package com.premiumminds.billy.core.services.builders.impl;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import javax.inject.Inject;
 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-/**
- * Annotate builder methods that can only be executed when the entity is not persisted.
- * 
- * @author Hugo Correia
- *
- */
-public @interface NotOnUpdate {
+public class BuilderManager<TBuilder extends AbstractBuilder<TBuilder, TType>, TType> {
 
-	String message() default "Enitity is marked as not new!";
+	private AbstractBuilder<TBuilder, TType>	builder;
+
+	@Inject
+	public BuilderManager(AbstractBuilder<TBuilder, TType> builder) {
+		this.builder = builder;
+	}
+
+	public void setTypeInstance(TType entity) {
+		builder.setTypeInstance(entity);
+	}
 }
