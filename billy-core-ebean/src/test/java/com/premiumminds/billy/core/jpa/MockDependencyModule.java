8df30dd524ff7a2e20e32b34a4868708db8f08f2
João Neves <joao.neves@premium-minds.com>
2017-08-07 11:36:57 +0100

    Updated the Copyright header of all source files

--- billy-core-ebean/src/test/java/com/premiumminds/billy/core/jpa/MockDependencyModule.java
+++ billy-core-ebean/src/test/java/com/premiumminds/billy/core/jpa/MockDependencyModule.java
@@ -1,20 +1,20 @@
 /**
  * Copyright (C) 2017 Premium Minds.
  *
- * This file is part of billy core JPA.
+ * This file is part of billy core Ebean.
  *
- * billy core JPA is free software: you can redistribute it and/or modify it under
+ * billy core Ebean is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
  *
- * billy core JPA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * billy core Ebean is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with billy core JPA. If not, see <http://www.gnu.org/licenses/>.
+ * along with billy core Ebean. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.premiumminds.billy.core.jpa;
 
