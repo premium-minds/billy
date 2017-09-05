b543a89cbf69cac3cc75c89eaf808acd944df4bd
João Neves <joao.neves@premium-minds.com>
2017-09-05 12:17:35 +0100

    Added tests to the rest of the Ebean queries of multiple DAO classes

---
+++ billy-core-ebean/src/test/java/com/premiumminds/billy/core/persistence/dao/jpa/DAOInvoiceSeriesImplTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2017 Premium Minds.
+ *
+ * This file is part of billy core Ebean.
+ *
+ * billy core Ebean is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * billy core Ebean is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with billy core Ebean. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.premiumminds.billy.core.persistence.dao.jpa;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.premiumminds.billy.core.persistence.entities.InvoiceSeriesEntity;
+import com.premiumminds.billy.core.persistence.entities.jpa.JPABusinessEntity;
+import com.premiumminds.billy.core.persistence.entities.jpa.JPAInvoiceSeriesEntity;
+import com.premiumminds.billy.core.services.UID;
+
+public class DAOInvoiceSeriesImplTest extends BaseH2Test {
+
+    private static String rightSeries = "Right Series";
+
+    private static String wrongSeries = "Wrong Series";
+
+    private static UID rightBusinessUid = new UID("f01970a9-c004-4f29-a3e1-bf2183248d76");
+
+    private static UID wrongBusinessUid = new UID("cde197fd-a866-4959-a2bc-6750360947d4");
+
+    private static UID invoiceSeriesUid = new UID("1796dc4d-462c-468c-9f0f-170b65944341");
+
+    private static DAOInvoiceSeriesImpl daoInvoiceSeriesImpl;
+
+    @Before
+    public void prepare() {
+        DAOBusinessImpl businessDAO = new DAOBusinessImpl();
+        businessDAO.beginTransaction();
+        JPABusinessEntity business = new JPABusinessEntity();
+        business.setUID(DAOInvoiceSeriesImplTest.rightBusinessUid);
+        business.setName("Test Business");
+        businessDAO.create(business);
+        businessDAO.commit();
+
+        DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl = new DAOInvoiceSeriesImpl();
+        DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.beginTransaction();
+        JPAInvoiceSeriesEntity series = new JPAInvoiceSeriesEntity();
+        series.setUID(DAOInvoiceSeriesImplTest.invoiceSeriesUid);
+        series.setBusiness(business);
+        series.setSeries(DAOInvoiceSeriesImplTest.rightSeries);
+        DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.create(series);
+        DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.commit();
+    }
+
+    @Test
+    public void getSeries() {
+        InvoiceSeriesEntity series = DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.getSeries(
+                DAOInvoiceSeriesImplTest.rightSeries, DAOInvoiceSeriesImplTest.rightBusinessUid.toString(), null);
+
+        Assert.assertEquals(series.getUID(), DAOInvoiceSeriesImplTest.invoiceSeriesUid);
+    }
+
+    @Test
+    public void getSeries_wrongSeriesId() {
+        InvoiceSeriesEntity series = DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.getSeries(
+                DAOInvoiceSeriesImplTest.wrongSeries, DAOInvoiceSeriesImplTest.rightBusinessUid.toString(), null);
+
+        Assert.assertEquals(series, null);
+    }
+
+    @Test
+    public void getSeries_wrongBusinessId() {
+        InvoiceSeriesEntity series = DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.getSeries(
+                DAOInvoiceSeriesImplTest.rightSeries, DAOInvoiceSeriesImplTest.wrongBusinessUid.toString(), null);
+
+        Assert.assertEquals(series, null);
+    }
+
+    @Test
+    public void getSeries_concurrent() {
+        /*DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.beginTransaction();
+        InvoiceSeriesEntity series =
+                DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.getSeries(DAOInvoiceSeriesImplTest.rightSeries,
+                        DAOInvoiceSeriesImplTest.rightBusinessUid.toString(), LockModeType.PESSIMISTIC_WRITE);
+        
+        series.setSeries("Changed Series");
+        DAOInvoiceSeriesImplTest.daoInvoiceSeriesImpl.commit();*/
+    }
+}
---
+++ billy-core-ebean/src/test/java/com/premiumminds/billy/core/persistence/dao/jpa/DAOSupplierImplTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2017 Premium Minds.
+ *
+ * This file is part of billy core Ebean.
+ *
+ * billy core Ebean is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * billy core Ebean is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with billy core Ebean. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.premiumminds.billy.core.persistence.dao.jpa;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.premiumminds.billy.core.persistence.entities.SupplierEntity;
+import com.premiumminds.billy.core.persistence.entities.jpa.JPASupplierEntity;
+import com.premiumminds.billy.core.services.UID;
+
+public class DAOSupplierImplTest extends BaseH2Test {
+
+    private static UID existingObjUid1 = new UID("1796dc4d-462c-468c-9f0f-170b65944341");
+
+    private static UID existingObjUid2 = new UID("a413c9e9-f2de-4f4b-a937-a63d88504796");
+
+    private static UID inactiveObjUid = new UID("f01970a9-c004-4f29-a3e1-bf2183248d76");
+
+    private static DAOSupplierImpl daoSupplierImpl;
+
+    @Before
+    public void prepare() {
+        DAOSupplierImplTest.daoSupplierImpl = new DAOSupplierImpl();
+    }
+
+    @Test
+    public void getAllActiveSuppliers_noSuppliers() {
+        List<SupplierEntity> suppliers = DAOSupplierImplTest.daoSupplierImpl.getAllActiveSuppliers();
+
+        Assert.assertEquals(suppliers.size(), 0);
+    }
+
+    @Test
+    public void getAllActiveSuppliers_noActiveSuppliers() {
+        DAOSupplierImplTest.daoSupplierImpl.beginTransaction();
+        JPASupplierEntity supplier = new JPASupplierEntity();
+        supplier.setUID(DAOSupplierImplTest.inactiveObjUid);
+        supplier.setName("Test Supplier 0");
+        DAOSupplierImplTest.daoSupplierImpl.commit();
+
+        List<SupplierEntity> suppliers = DAOSupplierImplTest.daoSupplierImpl.getAllActiveSuppliers();
+
+        Assert.assertEquals(suppliers.size(), 0);
+    }
+
+    @Test
+    public void getAllActiveSuppliers() {
+        DAOSupplierImplTest.daoSupplierImpl.beginTransaction();
+        JPASupplierEntity supplier = new JPASupplierEntity();
+        supplier.setUID(DAOSupplierImplTest.existingObjUid1);
+        supplier.setName("Test Supplier 1");
+        DAOSupplierImplTest.daoSupplierImpl.create(supplier);
+
+        supplier = new JPASupplierEntity();
+        supplier.setUID(DAOSupplierImplTest.existingObjUid2);
+        supplier.setName("Test Supplier 2");
+        DAOSupplierImplTest.daoSupplierImpl.create(supplier);
+        DAOSupplierImplTest.daoSupplierImpl.commit();
+
+        List<SupplierEntity> suppliers = DAOSupplierImplTest.daoSupplierImpl.getAllActiveSuppliers();
+
+        Assert.assertEquals(suppliers.size(), 2);
+    }
+}
