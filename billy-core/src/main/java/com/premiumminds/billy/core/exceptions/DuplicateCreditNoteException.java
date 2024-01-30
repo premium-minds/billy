7980f11c0a44ea17cf3ad26a6f1b54a1a755797d
apereira <antonio.pereira@premium-minds.com>
2024-01-30 11:41:56 +0000

    Fixed formatting on exception file

--- billy-core/src/main/java/com/premiumminds/billy/core/exceptions/DuplicateCreditNoteException.java
+++ billy-core/src/main/java/com/premiumminds/billy/core/exceptions/DuplicateCreditNoteException.java
@@ -21,8 +21,8 @@ package com.premiumminds.billy.core.exceptions;
 @Deprecated(forRemoval = true)
 public class DuplicateCreditNoteException extends BillyValidationException {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public DuplicateCreditNoteException() {
-	}
+    public DuplicateCreditNoteException() {
+    }
 }
